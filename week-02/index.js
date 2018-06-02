80c52886a8c93385549c46dc640b4617f0b59c19
David Buettner <dbuettner@hawk.iit.edu>
2018-06-02 12:38:37 -0500

    WEEK-02 problem working with index.js updated and response.json created when invoking 'node index.js'

--- week-02/index.js
+++ week-02/index.js
@@ -1,5 +1,6 @@
 // TODO: Read the contents of `README.md` before hacking on this file
-
+// Follow good programming practice with use strict
+'use strict';
 // Require the file system
 const fs = require('fs');
 // Require the request-promise-native package
@@ -9,13 +10,29 @@ const options = {
   // TODO: Create an object literal to hit your account on the GitHub API,
   // and pass any other information the API requires (you should not have
   // to authenticate for this, however)
+  uri: 'https://api.github.com/users/djbuettner',
+  headers: {
+    'User-Agent': 'Request-Promise'
+  },
+  json: true
 };
 
 req(options)
-  .then(
+  .then(function(response) {
     // TODO: Handle the returned JSON data and write it to a file called
     // `response.json` in your `week-two/` directory
+    console.log('JSON =', response);
+
+    // fs.writeFile('response.json', JSON.stringify(response).replace(',','\\n'), (err) => {
+    fs.writeFile('response.json', JSON.stringify(response, null, 2), (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log('File saved!');
+    } );
+  }
   )
   .catch(function(err) {
+    // The API failed and report error
     console.log(err);
   });
